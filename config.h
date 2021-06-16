b85cf3b9ff19b01a7d6eb17277dbdf528ebbff9a
ashincoder <ashinant15@gmail.com>
2021-06-16 07:35:04 +0530

    change color

--- config.h
+++ config.h
@@ -116,7 +116,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 1.0;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -138,7 +138,7 @@ static const char *colorname[] = {
     "#ebdbb2", [255] = 0,
     /* more colors can be added after 255 to use with DefaultXX */
     "#add8e6", /* 256 -> cursor */
-    "#555555", /* 257 -> rev cursor*/
+    "#a9b1d6", /* 257 -> rev cursor*/
     "#11121D", /* 258 -> bg */
     "#fff", /* 259 -> fg */
 };
