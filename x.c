7b441ff2ace70532cddb6c24845a6fe9c8c0aebe
ashincoder <ashinant15@gmail.com>
2021-07-01 22:12:54 +0530

    ligatures

--- x.c
+++ x.c
@@ -19,6 +19,7 @@ char *argv0;
 #include "arg.h"
 #include "st.h"
 #include "win.h"
+#include "hb.h"
 
 /* types used in config.h */
 typedef struct {
@@ -1040,6 +1041,9 @@ void xunloadfont(Font *f) {
 
 void xunloadfonts(void) {
 
+  /* Clear Harfbuzz font cache. */
+  hbunloadfonts();
+
   /* Free the loaded fonts in the font cache.  */
   while (frclen > 0)
     XftFontClose(xw.dpy, frc[--frclen].font);
@@ -1240,7 +1244,7 @@ int xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len,
     mode = glyphs[i].mode;
 
     /* Skip dummy wide-character spacing. */
-    if (mode == ATTR_WDUMMY)
+    if (mode & ATTR_WDUMMY)
       continue;
 
     /* Determine font for glyph if different from previous glyph. */
@@ -1345,6 +1349,9 @@ int xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len,
     xp += runewidth;
     numspecs++;
   }
+
+  /* Harfbuzz transformation for ligatures. */
+  hbtransform(specs, glyphs, len, x, y);
 
   return numspecs;
 }
@@ -1486,14 +1493,16 @@ void xdrawglyph(Glyph g, int x, int y) {
   xdrawglyphfontspecs(&spec, g, numspecs, x, y);
 }
 
-void xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og) {
+void xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og, Line line, int len) {
   Color drawcol;
 
   /* remove the old cursor */
   if (selected(ox, oy))
     og.mode ^= ATTR_REVERSE;
 
-  xdrawglyph(og, ox, oy);
+  /* Redraw the line where cursor was previously.
+   * It will restore the ligatures broken by the cursor. */
+  xdrawline(line, 0, oy, len);
 
   if (IS_SET(MODE_HIDE))
     return;
