828412c33ebbe56e46b60e07e573874b717ad57f
ashincoder <83629316+ashincoder@users.noreply.github.com>
2021-07-18 08:43:54 +0530

    added dracula colors

--- config.def.h
+++ config.def.h
@@ -118,38 +118,39 @@ float alpha = 0.9;
 const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#100f09", /* black   */
-  [1] = "#638B71", /* red     */
-  [2] = "#D8A93C", /* green   */
-  [3] = "#A1965C", /* yellow  */
-  [4] = "#226F8D", /* blue    */
-  [5] = "#5E9398", /* magenta */
-  [6] = "#9CAFA0", /* cyan    */
-  [7] = "#d7dbcc", /* white   */
+  [0] = "#000000", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bbbbbb", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#96998e",  /* black   */
-  [9]  = "#638B71",  /* red     */
-  [10] = "#D8A93C", /* green   */
-  [11] = "#A1965C", /* yellow  */
-  [12] = "#226F8D", /* blue    */
-  [13] = "#5E9398", /* magenta */
-  [14] = "#9CAFA0", /* cyan    */
-  [15] = "#d7dbcc", /* white   */
+  [8]  = "#44475a", /* black   */
+  [9]  = "#ff5555", /* red     */
+  [10] = "#50fa7b", /* green   */
+  [11] = "#f1fa8c", /* yellow  */
+  [12] = "#bd93f9", /* blue    */
+  [13] = "#ff79c6", /* magenta */
+  [14] = "#8be9fd", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
   /* special colors */
-  [256] = "#100f09", /* background */
-  [257] = "#d7dbcc", /* foreground */
-  [258] = "#d7dbcc",     /* cursor */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
 };
 
 /* Default colors (colorname index)
  * foreground, background, cursor */
- unsigned int defaultbg = 0;
+ unsigned int defaultbg = 256;
  unsigned int defaultfg = 257;
- unsigned int defaultcs = 258;
- unsigned int defaultrcs= 258;
+ unsigned int defaultcs = 257;
+ unsigned int defaultrcs= 257;
 
+ unsigned int defaultitalic = 7;
+ unsigned int defaultunderline = 7;
 /*
  * Default shape of cursor
  * 2: Block ("█")
@@ -157,7 +158,7 @@ const char *colorname[] = {
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
